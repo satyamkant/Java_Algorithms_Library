b4c727116f59a4f3470091dc20018beca8bc00e2
Satyam Kant <satyamkant2805@gmail.com>
2023-06-23 16:11:21 +0530

    Java learning and java DSA repo creation

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.* ;
+
+class solution{
+    void solve() {
+    }
+}
+
+class Main{
+    public static void main(String [] args){
+        solution sol = new solution();
+        sol.solve();
+
+    }
+}
+
+
+
---
+++ src/Oops.java
@@ -0,0 +1,13 @@
+
+class BaseClass{
+
+}
+
+/// extends keyword is used to inherit the properties
+/// or behaviour of the parent class (Inheritance)
+class DerivedClass extends BaseClass{
+
+}
+
+class Oops {
+}
