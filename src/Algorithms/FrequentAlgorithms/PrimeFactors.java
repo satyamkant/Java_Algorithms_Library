560503f4a7444a5c00b87263744b2752b0bcf8aa
Satyam kant <68018658+satyamkant@users.noreply.github.com>
2024-04-14 18:38:22 +0530

    Create PrimeFactors.java
    added prime factors class

---
+++ src/Algorithms/FrequentAlgorithms/PrimeFactors.java
@@ -0,0 +1,22 @@
+static class PrimeFactors{
+        /**
+         *
+         * @param n integer
+         * @return ArrayList of integers which are prime factors of n
+         * runs in O(Sqrt(n)) worst case (when n is prime)
+         */
+        private ArrayList<Long> calculate(long n){
+            ArrayList<Long> arr = new ArrayList<>();
+            for(long x = 2; x * x <=  n; x++){
+                while(n%x == 0){
+                    arr.add(x);
+                    n/=x;
+                }
+            }
+            if(n > 1)arr.add(n);
+            return arr;
+        }
+        public ArrayList<Long> getFactors(long n){
+            return calculate(n);
+        }
+    }
