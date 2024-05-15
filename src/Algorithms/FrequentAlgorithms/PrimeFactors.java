04deb6003f1af20120475f5b20e86b739798a722
Satyam kant <68018658+satyamkant@users.noreply.github.com>
2024-05-15 11:33:23 +0530

    Update PrimeFactors.java
    added class to calculate prime factors in log(N)

--- src/Algorithms/FrequentAlgorithms/PrimeFactors.java
+++ src/Algorithms/FrequentAlgorithms/PrimeFactors.java
@@ -19,4 +19,45 @@ class PrimeFactors{
         public ArrayList<Long> getFactors(long n){
             return calculate(n);
         }
+}
+
+// prime factors in repeated query log(N)
+class PrimeFactors{
+        private final int N;
+        private final int[] spf;
+
+        private void sieve(){
+            for(int i = 2;i < N;i++){
+                if (spf[i] == 1)
+                {
+                    spf[i] = i;
+                    for (long j = (long) i * i; j <= N; j += i)
+                    {
+                        if (spf[(int) j] == 1)spf[(int) j] = i;
+                    }
+                }
+            }
+        }
+
+        PrimeFactors() {
+            this.N = 1000005;
+            this.spf = new int[N+1];
+            for(int i = 0;i < N;i++){
+                spf[i] = 1;
+            }
+            sieve();
+        }
+
+        List<Integer> getPrimeFactors(int n){
+            List<Integer> ans = new LinkedList<>();
+            while(n > 1){
+                int fact = spf[n];
+                while(n % fact == 0){
+                    n /= fact;
+                    ans.add(fact);
+                }
+            }
+            return ans;
+        }
     }
+
